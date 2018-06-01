c83af42b93ceae4d503fd2584fa04e418c92e9a6
chenjiaxi <chenjiaxi_byr@163.com>
2018-06-01 14:13:38 +0800

    设备模型接口

--- controllers/api_devicesModel.js
+++ controllers/api_devicesModel.js
@@ -11,4 +11,72 @@ module.exports = {
 
         ctx.rest(res)
     },
+
+    'GET /api/dModel/getSitedModel/:siteId': async (ctx, next) => {  //通过站点Id获取
+        var siteId = ctx.params.siteId;
+        var res = await dModel.getSitedModel(siteId);     
+        ctx.rest({
+            dModels: res
+        });
+    },
+
+
+    'DELETE /api/dModel/dModelDelete/:deviceId': async (ctx, next) => {   //删除站点dmodel，失败res=0,成功=1
+        console.log(`delete dModel ${ctx.params.deviceId}...`);
+        var s = await dModel.dModelDelete(ctx.params.deviceId);
+        if (s) {
+            ctx.rest({
+                res:s
+            });
+        } else {
+            throw new APIError('dModel:not_found', 'dModel not found by deviceId.');
+        }
+    },
+
+    'PUT /api/dModel/dModelRename/:deviceId': async (ctx,next) => {
+        var deviceId = ctx.params.deviceId;
+        var newname = ctx.request.body.name;
+        var s = await dModel.dModelRename(deviceId,newname);
+        if (s.res[0] === 1) {
+            ctx.rest(s);
+        } else {
+            throw new APIError('dModel:not_found', 'dModel not found by deviceId.');
+        }
+    },
+
+    'PUT /api/dModel/dModelUrl/:deviceId': async (ctx,next) => {
+        var deviceId = ctx.params.deviceId;
+        var dModelUrl = ctx.request.body.url;
+        var s = await dModel.dModelUrl(deviceId,dModelUrl);
+        if (s.res[0] === 1) {
+            ctx.rest({
+                res:s
+            });
+        } else {
+            throw new APIError('dModel:not_found', 'dModel not found by deviceId.');
+        }
+    },
+
+    'PUT /api/dModel/dModelLocation/:deviceId': async (ctx,next) => {
+        var deviceId = ctx.params.deviceId;
+        var location = ctx.request.body.location;
+        var s = await dModel.dModelLocation(deviceId,location);
+        if (s.res[0] === 1) {
+            ctx.rest(s);
+        } else {
+            throw new APIError('dModel:not_found', 'dModel not found by deviceId.');
+        }
+    },
+
+    'PUT /api/dModel/addLabel/:deviceId': async (ctx,next) => {
+        var deviceId = ctx.params.deviceId;
+        var label = ctx.request.body.label;
+        var s = await dModel.addLabel(deviceId,label);
+        if (s.res[0] === 1) {
+            ctx.rest(s);
+        } else {
+            throw new APIError('dModel:not_found', 'dModel not found by deviceId.');
+        }
+    },
+    
 }
--- controllers/api_sites.js
+++ controllers/api_sites.js
@@ -77,21 +77,7 @@ module.exports = {
         }
     },
 
-    /*  参数：即url后面/:id部分，一个/test/中间只有一个参数，？后面接的是query串，在ctx.query里面
-       可以拿到url中的query参数和值。*/
-    // 'GET /api/test/:id': async (ctx,next) =>{
-    //     var id = ctx.params.id;
-    //     var name = ctx.query.name;
-        
-    //     console.log(id+' '+name);
-    // },
-    // 'GET /api/sites': async (ctx, next) => {
-    //          //通过await执行promise对象，拿到结果
-    //     var res = await scenes.getSites(); 
-    //     console.log('cnm');
-    //     ctx.rest({
-    //         scenes: res
-    //     });
-    // },
+    
+    
 
 };
